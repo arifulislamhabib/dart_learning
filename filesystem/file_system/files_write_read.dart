c84eb85e94c5e220664d38eafa5b10c5cf523254
arifulislamhabib <arifulislamhabib@gmail.com>
2025-11-06 17:27:15 +0600

    path and directory check and write to the file

--- filesystem/file_system/files_write_read.dart
+++ filesystem/file_system/files_write_read.dart
@@ -1,6 +1,21 @@
 import 'dart:io';
 Future<int> main()async
 {
+  // create files path.
+  final file = File('Hello.txt');
+
+  //check current directory & path.
+
+  //this will show name of the value keep in the  *file* variable.
+  print('working with file:${file.path}');
+
+  // It will show upto this point full path
+  print('Current Directory: ${Directory.current.path}');
+
+
+  //write in the file
+  await file.writeAsString('Hello world');
+  print('Wrote hello world to the the file: ${file.path}');
 
 
   return 0 ;
